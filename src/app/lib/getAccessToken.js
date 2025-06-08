1171a4af16e139724e8c3bcca2ebe736ab936467
Sayan Biswas <sayanbiswas6073@gmail.com>
2025-06-08 16:29:23 +0530

    access token fetching bug fixed

--- src/app/lib/getAccessToken.js
+++ src/app/lib/getAccessToken.js
@@ -1,29 +1,48 @@
-import qs from "qs"
+import qs from "qs";
 import axios from "axios";
 
 let access_token = null;
 
-const getAccessToken = async() => {
-  // if(access_token != null) {
-  //   console.log(new Date(access_token.expires_at))
-  //   return access_token.access_token
-  // } else {
-    return await axios.post("https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token", qs.stringify({
-      "client_id": "TEST-M23O5AF1YO7TE_25060",
-      "client_version": 1,
-      "client_secret": "NzJjMDYzNTYtYzYwYi00MjJkLWFhZTEtMWIyMWMxMWRkNzcy",
-      "grant_type": "client_credentials"
-    }), {
-      headers:{
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }}).then(res=> {
-      access_token = res.data;
-      // console.log(new Date( Date.now() + access_token.expires_in), new Date())
-      return res.data.access_token;
-    }).catch(err=> {
-      console.log(err)
-    })
-  // }
-}
+/**
+ * Fetches and caches the access token.
+ * Reuses the token if not expired.
+ */
+const getAccessToken = async () => {
+  const now = Date.now();
+
+  // Reuse token if not expired
+  if (access_token && access_token.expires_at > now) {
+    return access_token.access_token;
+  }
+
+  try {
+    const response = await axios.post(
+      "https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token",
+      qs.stringify({
+        client_id: "TEST-M23O5AF1YO7TE_25060",
+        client_version: 1,
+        client_secret: "NzJjMDYzNTYtYzYwYi00MjJkLWFhZTEtMWIyMWMxMWRkNzcy",
+        grant_type: "client_credentials",
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const data = response.data;
+    // Cache the token with expiry
+    access_token = {
+      access_token: data.access_token,
+      expires_at: now + (data.expires_in || 0) * 1000, // Convert to ms
+    };
+
+    return access_token.access_token;
+  } catch (err) {
+    console.error("Failed to fetch access token:", err.message || err);
+    throw err;
+  }
+};
 
 export default getAccessToken;
